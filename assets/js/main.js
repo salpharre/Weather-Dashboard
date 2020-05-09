47901a6e2d0b000f4d23d6a1c2e9f9c0ea2e2048
Sandra <sandra.alpha2010@gmail.com>
2020-05-09 15:57:44 -0700

    if have time will attempt local storage but focusing in on other features, rearranging layout to add moment library for date and time

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,27 @@
 var userLocation;
 var valueToday;
 
-//$("#weatherLocation").val(JSON.parse(localStorage.getItem("location")));
+// function getItem(){
+//     $("#").val(localStorage.getItem("nest"));
+// }
 
-//$("")
 
+// $("#nest").val(localStorage.getItem("nest0"));
+// $("#nest").val(localStorage.getItem("nest1"));
+// $("#nest").val(localStorage.getItem("nest2"));
+// $("#nest").val(localStorage.getItem("nest3"));
+// $("#nest").val(localStorage.getItem("nest4"));
+
+// $("#weatherLocation").val(localStorage.getItem("location"));
+/*
+$("#0").val(JSON.parse(localStorage.getItem("number")));
+$("#1").val(JSON.parse(localStorage.getItem("number")));
+$("#2").val(JSON.parse(localStorage.getItem("number")));
+$("#3").val(JSON.parse(localStorage.getItem("number")));
+$("#4").val(JSON.parse(localStorage.getItem("number")));
+$("#5").val(JSON.parse(localStorage.getItem("number")));
+$("#6").val(JSON.parse(localStorage.getItem("number")));
+*/
 $("#search").on("click", function(e){
 
 
@@ -18,6 +35,9 @@ $("#search").on("click", function(e){
     // $("#todayCard").empty();
     // $(".row").empty();
     
+    //getItem();
+
+
     //refer to the global var userLocation
 
     var userLocation = $("#location").val();//value is inserted into city for queryURLCurrent
@@ -93,9 +113,10 @@ $("#search").on("click", function(e){
         var w = $('<div id="w"></div>').text(weather);
     
         $("#nest").append(w, cTF, fLF, tMF, tMinF)
-    
-        
 
+        
+        /*var for url key in ajax call, uses the same APIKey as used by current weather.
+        lat and lon are taken from the response of current weather for the city entered*/
         var queryURLSeven = "https://api.openweathermap.org/data/2.5/onecall?lat="
         + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=" + APIKey;
 
@@ -108,68 +129,79 @@ $("#search").on("click", function(e){
             var daily = response.daily;
             //console.log(daily);
 
-            var tempDay;
-            var tempEve;
-            var tempMax;
-            var tempMin;
-            var weather;
+            // var tempDay;
+            // var tempEve;
+            // var tempMax;
+            // var tempMin;
+            // var weather;
 
-            for (var i = 0; i < daily.length; i++){
+            for (var i = 0; i < (daily.length - 1); i++){
                 var tempDay = (daily[i].temp.day - 273) * 1.80 + 32;
                 //make div and append
                 var tempEve = (daily[i].temp.eve - 273) * 1.80 + 32;
-                var tempMax = (daily[i].temp.eve - 273) * 1.80 + 32;
-                var tempMin = (daily[i].temp.eve - 273) * 1.80 + 32;
-                var weather = daily[i].weather[0].description;
+                var tempMax = (daily[i].temp.max - 273) * 1.80 + 32;
+                var tempMin = (daily[i].temp.min - 273) * 1.80 + 32;
+                var dayWeather = daily[i].weather[0].description;
+
+               var tD = $("<div id='day'></div>").text("Daytime: " + tempDay.toFixed(0)); 
+               var tE = $("<div id='eve'></div>").text("Evening: " + tempEve.toFixed(0));
+               var tMax = $("<div id='dayMax'></div>").text("Max: " + tempMax.toFixed(0));
+               var tMin = $("<div id='dayMin'></div>").text("Min: " + tempMin.toFixed(0));
+               var dW = $("<div id='dayweather'></div>").text("Weather: " + dayWeather);
+
+                $("#" + [i]).append(dW, tD, tE, tMax, tMin);
 
 
+                //var numberValue = $("#" + [i]).val("#day", "#eve", "#dayMax", "#dayMin", "#dayweather");
 
-            console.log(tempDay);
-            console.log(tempEve);
-            console.log(tempMax);
-            console.log(tempMin);
-            console.log(weather);
-
+                //JSON.stringify(localStorage.setItem("number", numberValue))
             }
             
 
         });
         
+        
     });
 
     
-    // JSON.stringify(localStorage.setItem("location", userLocation));
+    // localStorage.setItem("location", userLocation);
     
-    // var valueToday = $("div#w").val();
-    // console.log(valueToday)
+    // var nestFeels = $("#feels").text();
+    // var nestW = $("#w").text();
+    // var nestCurrent = $("#current").text();
+    // var nestMax = $("#max").text();
+    // var nestMin = $("#min").text();
 
-    // JSON.stringify(localStorage.setItem("jumbo", valueToday));
+    // localStorage.setItem("nest0", nestFeels);
+    // localStorage.setItem("nest1", nestW);
+    // localStorage.setItem("nest2", nestCurrent);
+    // localStorage.setItem("nest3", nestMax);
+    // localStorage.setItem("nest4", nestMin);
     
-})
-
-
-//checking to see if ajax works and what to grab 
-
-
-//call current weather first, it spits out longitude and latitude, grab those two and incert into lat and lon for onecall
-
-
-//function renderForecast{} ---have both api calls in the same one, current weather first and then 7 day
-
-//var test = "Davis,California"
+});
 
 
 
+
+
+
+// function setNestIds(){
+    
+// }
+
+
+
+
+
+//var nest
+
+    //trying to grab all the text in each div (with ids) that is nested under the div with id of nest
+    //var nestValue = JSON.stringify($("#nest").html());
+    //console.log(nestValue)
+
+    //localStorage.setItem("nest", nestValue)
 
         
-
-//var city = "San Francisco,us"
-
-var lat = ["37.77"];
-
-var lon = ["-122.42"];
-
-
 
 
 
