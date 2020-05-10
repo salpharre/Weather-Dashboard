f7273e71839928d11d8994515cbd4e92eb1cfcb6
Sandra <sandra.alpha2010@gmail.com>
2020-05-09 23:55:44 -0700

    finished commenting and readme except for link

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Weather-Dashboard
 
-
+Dashboard for your location's weather, was created using OpenWeatherMap API.
 
 ## Table of Contents
 
@@ -8,23 +8,27 @@
 
 * Technology
 
-* Library
-
 * Launch
 
 ### General Info
 
+This is a dashboard for 'Your Weather'. Simply type in your location, city and state and watch as current weather and a seven day forecast is born. 
 
 
 #### Technology
 
+* Visual Studio Code 1.45.0
 
+* Bootstrap
 
-#### Library
+* Moment.js
 
-
+* OpenWeathermap.org API
 
 ##### Launch
+
+Original Launch Date: `May 9th, 2020`
+
 
 
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,94 +1,113 @@
 
 
-/**/
+/*The functions that create the moment and tie it to a div on the html is called 
+here so it renders onto the webpage as soon as it loads*/
 
 currentDay();
 
 currentHour();
 
+
+/* Initialize var to rename later in the event listener */
 var userLocation;
 
-
+/*This is an event listener for the search btn, it takes the value in the search input and does two things.
+One, it prints it to the webpage for the user and two, it sets a variable to it (initialized in global scope) to be used be
+concatenated into the url for openweathermap api*/
 
 $("#search").on("click", function(e){
 
-
+/*prevents the document from doing what it usually does, prevents it from refreshing. Want to do this because want to use the value the 
+button will be grabbing from the search input to use in the first ajax call*/
     e.preventDefault();
 
+/*Empties the two divs for current day's weather and 7 day forecast weather so that when another value is entered into the input and the button is pressed
+it won't add a new set of divs on top of the old ones*/
     $("#nest").empty();
     $(".forecast").empty();
     
 
-
-    var userLocation = $("#location").val();//value is inserted into city for queryURLCurrent
+/*sets the variable, that was initialized in global scope, to the value that was entered into the input search.
+this value is then concantenated into the url used for the first ajax call*/
+    userLocation = $("#location").val();
     
 
-    /*grabs the id of the span element located under div with an id of todayCard,
+    /*grabs the id of the span element located under a div with a class of city,
     it grabs the id in order to add the user Input from the search bar to the webpage
     */
     $("#weatherLocation").text(userLocation)
 
-
-    // var storeWeather = $("#weatherLocation").val();
-    // console.log(storeWeather);
     
     
-    // put both ajaxes inside here
+    /* declares the apikey to be used in the url for the first ajax call */
     var APIKey = "adcd424f400eb6c61768801157796f11";
 
+    /* the url to be used in the first ajax call */
     var queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q="
     + userLocation + "&appid=" + APIKey;
     
+    /* the first ajax call, the coordinates, longitude and langitude, are taken from this response and concantenated into the url for the second ajax call (for the seven day forecast) */
     $.ajax({
         url: queryURLCurrent,
         method: "GET"
     }).then(function(response){
         console.log(response);
     
-        
+        /* variables to hold the response for the coordinates longitude and langitude */
         var lon = response.coord.lon
         var lat = response.coord.lat 
         
     
-    
+        /*All var with response... are holding a response for the associated temperature, except weather, which is holding the current sky conditions*/
+
+
+        /*holds the response for the feels_like temperature*/
         var feelsLike = response.main.feels_like;
+
+        /*turns the response for feels_like into farenheit*/
         var feelsLikeF = (feelsLike - 273.15) * 1.80 + 32;
     
         /*Creates a variable that will be appended later to represent
-        a newly created div, with an id of feels for styling anchor, that
-        prints the rounded temp number to nestle under card-body div for current weather*/
-        var fLF = $('<div id="feels"></div>').text("Feels Like: " + feelsLikeF.toFixed(0));
+        a newly created div, with an class of feels for styling anchor, that
+        prints the rounded temp number to nestle under nest div for current weather*/
+        var fLF = $('<div>').addClass("feels").text("Feels Like: " + feelsLikeF.toFixed(0));
       
     
         var currentTemp = (response.main.temp - 273.15) * 1.80 + 32;;
        
     
-        var cTF = $('<div id="current"></div>').text("Temperature (F): " + currentTemp.toFixed(0));
+        var cTF = $('<div>').addClass("current").text("Temperature (F): " + currentTemp.toFixed(0));
     
         var tempMax = (response.main.temp_max - 273.15) * 1.80 + 32;
         
     
-        var tMF = $('<div id="max"></div>').text("High: " + tempMax.toFixed(0));
+        var tMF = $('<div>').addClass("max").text("High: " + tempMax.toFixed(0));
         
         var tempMin = (response.main.temp_min - 273.15) * 1.80 + 32;
     
 
-        var tMinF = $('<div id="min"></div>').text("Low: " + tempMin.toFixed(0));
+        var tMinF = $('<div>').addClass("min").text("Low: " + tempMin.toFixed(0));
         
         var weather = response.weather[0].main;
 
 
 
-        var w = $('<div id="w"></div>').text(weather);
+        var w = $('<div>').addClass("w").text(weather);
+
+        /*concatenates the response with the url for the icon .png and is held in a variable*/
 
         var iconCurrent = "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+
+        /*new element of img is created with an attribute of src to hold the url picture for the icon (that changes based on the weather conditions)*/
         var iC = $("<img>").attr("src", iconCurrent);
     
+
+        /*appends all variables that were created with a class and text are appended to the parent div with a id of nest*/
         $("#nest").append(iC, w, cTF, fLF, tMF, tMinF)
 
         
         /*var for url key in ajax call, uses the same APIKey as used by current weather.
-        lat and lon are taken from the response of current weather for the city entered*/
+        lat and lon are taken from the response of current weather for the city entered (in the search input)*/
         var queryURLSeven = "https://api.openweathermap.org/data/2.5/onecall?lat="
         + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=" + APIKey;
 
@@ -98,10 +117,14 @@ $("#search").on("click", function(e){
         }).then(function(response){
             console.log(response);
 
+
+            /*holds the response for daily weather*/
             var daily = response.daily;
             
 
-
+            /*for loop to loop through the array of daily. everytime it loops six variables are created to hold the temp, weather conditions, and icon image.
+            these are then used to create new divs, and an img element where classes are added and text added. These are then appended to each card (seven in total) everytime
+            the for loop loops*/
             for (var i = 0; i < (daily.length - 1); i++){
                 var tempDay = (daily[i].temp.day - 273) * 1.80 + 32;
                 
@@ -114,11 +137,11 @@ $("#search").on("click", function(e){
 
 
 
-               var tD = $("<div id='day'></div>").text("Daytime (F): " + tempDay.toFixed(0)); 
-               var tE = $("<div id='eve'></div>").text("Evening: " + tempEve.toFixed(0));
-               var tMax = $("<div id='dayMax'></div>").text("Max: " + tempMax.toFixed(0));
-               var tMin = $("<div id='dayMin'></div>").text("Min: " + tempMin.toFixed(0));
-               var dW = $("<div id='dayweather'></div>").text(dayWeather);
+               var tD = $("<div>").addClass("d").text("Daytime (F): " + tempDay.toFixed(0)); 
+               var tE = $("<div>").addClass("eve").text("Evening: " + tempEve.toFixed(0));
+               var tMax = $("<div>").addClass("dayMax").text("Max: " + tempMax.toFixed(0));
+               var tMin = $("<div>").addClass("dayMin").text("Min: " + tempMin.toFixed(0));
+               var dW = $("<div>").addClass("dayweather").text(dayWeather);
 
                var iC = $("<img>").attr("src", iconDaily);
 
@@ -136,10 +159,12 @@ $("#search").on("click", function(e){
     
 });
 
+/* initializing the vars for moment library */
 var mDay;
 var mHour;
 
-
+/*functions that are to be called right when the page loads. One creates a variable to hold the moment for day and adds it to the webpage via the id and the other
+also creates a variable to hold the moment for the hour and adds it to the webpage via the id.*/
 function currentDay(){
     var mDay = moment().format("LL");
 
